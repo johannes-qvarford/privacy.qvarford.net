3829cbb456e5ba8003503aafbf10fd9e6661c97f
Johannes Qvarford <jq.email+github@pm.me>
2023-04-14 21:04:05 +0200

    Ported invidious to ansible.

--- tocopy/scripts/apps/update-invidious.sh
+++
@@ -1,9 +0,0 @@
-source tocopy/scripts/utils.sh
-
-cd-app invidious
-docker-compose -f docker-compose-prod.yml down
-git stash
-git pull
-docker-compose -f docker-compose-prod.yml pull
-sync-app invidious
-docker-compose -f docker-compose-prod.yml up -d
